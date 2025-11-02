1254fe55dce0204dddb18c8a0e676060c76115c3
Benjamin Owusu <owusubenjamin446@gmail.com>
2025-11-02 21:23:47 +0000

    Creating Specific components in the React App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,12 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <WelcomeMessage /> {/* This renders the custom component */}
+      </div>
+      <div>
+        <Header /> {/* Top Section */}
+        <MainContent /> {/* Middle Section */}
+        <Footer /> {/* Bottom Section */}
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
