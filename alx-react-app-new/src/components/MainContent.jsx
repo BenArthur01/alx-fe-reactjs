b3be5baa3ca79cd104e520c29ed7aa48a134d484
Benjamin Owusu <owusubenjamin446@gmail.com>
2025-11-08 22:15:01 +0000

    Applying inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+// src/MainContent.jsx
+
+function MainContent() {
+    return (
+        <main style={{ backgroundColor: '#f0f0f0', padding: '15px', fontSize: '18px'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
---
+++ alx-react-app-new/src/components/WelcomeMessage.jsx
@@ -0,0 +1,13 @@
+// This is a simple functional component using JSX
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Hello everyone, I am learning React at ALX!</h1> {/* Updated heading */}
+      <p>This is a simple JSX component.</p>
+      <p>I am learning about JSX!</p> {/* New paragraph added */}
+    </div>
+  );
+}
+
+// Export the component so it can be used in other files
+export default WelcomeMessage;
